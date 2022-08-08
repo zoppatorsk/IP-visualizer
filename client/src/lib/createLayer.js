86c9725f844191167fa2b01c9c5a69e9e278cf89
zoppatorsk <49608884+zoppatorsk@users.noreply.github.com>
2022-08-08 18:23:20 +0200

    migrate to maplibre

--- client/src/lib/createLayer.js
+++ client/src/lib/createLayer.js
@@ -38,6 +38,10 @@ export default function createLayer(selectedLayer, ipData) {
 				elevationScale: get(layerSettings)[selectedLayer].elevationScale,
 				elevationRange: get(layerSettings)[selectedLayer].elevationRange,
 				getPosition: (d) => d.coordinates,
+				getElevationVeight: (d) => d.hosts * 10,
+				getColorWeight: (d) => ipData.filter((ip) => ip.coordinates[0] === d.coordinates[0] && ip.coordinates[1] === d.coordinates[1]).reduce((acc, cur) => acc + cur.hosts, 0),
+				pickable: true,
+
 				// 	[0, 'rgba(0, 0, 255, 0.2)'],
 				// 	[1, 'rgba(0, 0, 255, 0.2)'],
 				// ],
@@ -57,14 +61,16 @@ export default function createLayer(selectedLayer, ipData) {
 				radiusScale: get(layerSettings)[selectedLayer].radiusScale,
 				lineWidthMinPixels: get(layerSettings)[selectedLayer].lineWidthMinPixels,
 				getPosition: (d) => d.coordinates,
-				getFillColor: (d) => [255, 140, 0],
+				// getFillColor: (d) => [1, 'rgba(0, 0, 255, 0.2)'],
+				getFillColor: (d) => colorByHosts(d.hosts),
 				pickable: true,
-				// onHover: (info) => info && showTooltip(info),
-				getTooltip: ({ object }) => object && object.CIDR,
-				onClick: (info) => console.log(info.object),
+				onHover: (info) => info.object && document.dispatchEvent(new CustomEvent('info', { bubbles: true, detail: info.object })),
+				// onHover: (info) => info && document.dispatchEvent(new CustomEvent('info', { bubbles: true, detail: { data: info.object, x: info.x, y: info.y } })),
+
+				// onClick: (info) => console.log(info.object),
 
 				// 	[0, 'rgba(0, 0, 255, 0.2)'],
-				// 	[1, 'rgba(0, 0, 255, 0.2)'],
+				// 	,
 				// ],
 			});
 			break;
@@ -74,6 +80,13 @@ export default function createLayer(selectedLayer, ipData) {
 	}
 	return layer;
 }
-function showTooltip(info) {
-	console.log(info);
+
+//fix these colors later
+function colorByHosts(hosts) {
+	if (hosts >= 50000) return [132, 94, 194];
+	if (hosts >= 10000) return [214, 93, 177];
+	if (hosts >= 5000) return [255, 150, 113];
+	if (hosts > 1024) return [255, 150, 113];
+	if (hosts >= 512) return [255, 199, 95];
+	if (hosts > 0) return [249, 248, 113];
 }
